7e9740dadaaa5066b53bf97c77778c44f74587d2
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-11-14 21:15:41 +0200

    Initial Commit

---
+++ agoratest/agoratestApp.swift
@@ -0,0 +1,17 @@
+//
+//  agoratestApp.swift
+//  agoratest
+//
+//  Created by Даниил Крымлов on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct agoratestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
